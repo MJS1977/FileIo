41a728757f1d822cbcfd9b175bf13a9b7d558b36
mikko.siukola <mikko.siukola@5CG05206QW.nordic.local>
2021-01-26 04:21:32 +0200

    chore: initial commit

---
+++ src/io/Inputhandler.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.util.Scanner;
+
+public class Inputhandler {
+
+    public Integer getChoise(Scanner scanner) {
+
+        return scanner.nextInt();
+    }
+
+    public String getWord(Scanner scanner){
+       
+    
+        return scanner.nextLine();
+    }
+    }
+    
+
